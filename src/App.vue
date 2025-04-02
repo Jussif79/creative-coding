871bd6cba04f37d3b1c7105ec187cdeee6e672fa
Jussif79 <jussif.79@gmail.com>
2025-04-02 11:36:53 +0200

    Add Style

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,37 @@ import {
 <template>
 <header>
     <div>
-        <nav class="flex justify-center items-center mt-8 space-x-4">
-            <RouterLink to="/" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+        <nav class="flex justify-center items-center mt-8 space-x-4 bg-green-400">
+            <RouterLink to="/" class="px-4 py-2 hover:bg-green-100 rounded transition text-indigo-400">
                 Home
             </RouterLink>
 
             <div class="relative group">
-                <RouterLink to="/woche1" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche1" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 1
                 </RouterLink>
 
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
-                    <RouterLink to="/woche1/sketch1" class="block px-4 py-2 text-sm hover:bg-gray-100 rounded-t-md">
+                    <RouterLink to="/woche1/sketch1" class="block px-4 py-2 text-sm hover:bg-green-100 rounded-t-md">
                         Sketch1
                     </RouterLink>
-                    <RouterLink to="/woche1/sketch2" class="block px-4 py-2 text-sm hover:bg-gray-100">
+                    <RouterLink to="/woche1/sketch2" class="block px-4 py-2 text-sm hover:bg-green-100">
                         Sketch2
                     </RouterLink>
-                    <RouterLink to="/woche1/sketch3" class="block px-4 py-2 text-sm hover:bg-gray-100">
+                    <RouterLink to="/woche1/sketch3" class="block px-4 py-2 text-sm hover:bg-green-100">
                         Sketch3
                     </RouterLink>
-                    <RouterLink to="/woche1/sketch4" class="block px-4 py-2 text-sm hover:bg-gray-100">
+                    <RouterLink to="/woche1/sketch4" class="block px-4 py-2 text-sm hover:bg-green-100">
                         Sketch4
                     </RouterLink>
-                    <RouterLink to="/woche1/image-filter" class="block px-4 py-2 text-sm hover:bg-gray-100 rounded-b-md">
+                    <RouterLink to="/woche1/image-filter" class="block px-4 py-2 text-sm hover:bg-green-100 rounded-b-md">
                         Image Filter
                     </RouterLink>
                 </div>
             </div>
 
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 2
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -46,7 +46,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 3
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -54,7 +54,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 4
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -62,7 +62,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 5
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -70,7 +70,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 6
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -78,7 +78,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 7
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -86,7 +86,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 8
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
@@ -94,7 +94,7 @@ import {
                 </div>
             </div>
             <div class="relative group">
-                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-gray-100 rounded transition">
+                <RouterLink to="/woche2" class="px-4 py-2 hover:bg-green-100 rounded transition">
                     Woche 9
                 </RouterLink>
                 <div class="absolute left-0 mt-1 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[160px] z-10">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-  </main>
+<main>
+        <div class="mt-15 flex justify-center">
+            <h1 class="text-3xl font-mono font-bold mb-2 text-indigo-400">p5.js Sketches</h1>
+        </div>
+        <div class="flex justify-center">
+          <p class="text-green-400">Eine Sammlung kreativer Codestücke</p>
+        </div>
+</main>
 </template>
